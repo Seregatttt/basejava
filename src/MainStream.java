a2d5d6202db152be72d2462d0773d7b43211dd2d
seregatttt <seregatttt@mail.ru>
2020-02-01 11:13:58 +0300

    HW12.3 corrected the mentor's comment

--- src/MainStream.java
+++ src/MainStream.java
@@ -5,23 +5,18 @@ import java.util.stream.Stream;
 public class MainStream {
 
 	public static void main(String[] args) {
-
 		System.out.println(minNumber(new ArrayList<>(Arrays.asList(1, 1, 2, 2, 3, 3, 1, 1, 1, 1))));
 		System.out.println(oddOrEven(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))));
 	}
 
 	private static int minNumber(List<Integer> integers) {
-		Optional<Integer> s = integers
+		Optional<Integer> s = Optional.ofNullable(integers
 				.stream()
 				.distinct()
 				.sorted()
-				.reduce((s1, s2) -> s1 * 10 + s2);
+				.reduce(0, (s1, s2) -> s1 * 10 + s2));
 
-		if (s.isPresent()) {
-			return s.get();
-		} else {
-			return 0;
-		}
+		return s.orElse(0);
 	}
 
 	private static List<Integer> oddOrEven(List<Integer> integers) {
@@ -29,13 +24,18 @@ public class MainStream {
 				.stream()
 				.collect(Collectors.partitioningBy((p) -> p % 2 == 0));
 
-		Optional<Integer> sum = Stream.concat(groupListNumber.get(true).stream(), groupListNumber.get(false).stream())
-				.reduce((s1, s2) -> s1 + s2);
+		Optional<Integer> sum = Optional.ofNullable(
+				Stream.concat(groupListNumber.get(true).stream(), groupListNumber.get(false).stream())
+						.reduce(0, (s1, s2) -> s1 + s2));
 
-		if (sum.get() % 2 == 0) {
-			return groupListNumber.get(true);
+		if (sum.isPresent()) {
+			if (sum.get() % 2 == 0) {
+				return groupListNumber.get(true);
+			} else {
+				return groupListNumber.get(false);
+			}
 		} else {
-			return groupListNumber.get(false);
+			return null;
 		}
 	}
 }
